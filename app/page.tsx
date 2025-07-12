3ad73402e966104dfcc9fa1d7fc7655ac7217f47
xin <anxanxx@gmail.com>
2025-07-12 20:22:03 +0800

    feat(page): enhance button variants and update global styles for Figma integration

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { ThemeToggle } from '@/components/theme-toggle';
 import { Button } from '@/registry/ui3/ui/button';
+import { PlusIcon } from 'lucide-react';
 
 // This page displays items from the custom registry.
 // You are free to implement this with your own design as needed.
@@ -22,9 +23,23 @@ export default function Home() {
         <div className='flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative'>
           <div className='flex items-center justify-between'>
             <h2 className='text-sm text-muted-foreground sm:pl-3'>Button</h2>
+            <span className='font-mono text-xs'>
+              npx shadcn@latest add https://figui.dev/r/button.json
+            </span>
           </div>
           <div className='flex flex-col gap-4 items-center justify-center min-h-[400px] relative '>
-            <Button variant='ghost'>Click me</Button>
+            <Button variant='primary'>Button</Button>
+            <Button variant='secondary'>Button</Button>
+            <Button variant='destructive'>Button</Button>
+            <Button variant='secondaryDestruct'>Button</Button>
+            <Button variant='inverse'>Button</Button>
+            <Button variant='success'>Button</Button>
+            <Button variant='link'>Button</Button>
+            <Button variant='linkDanger'>Button</Button>
+            <Button variant='ghost'>Button</Button>
+            <Button variant='ghost' size='icon'>
+              <PlusIcon />
+            </Button>
           </div>
         </div>
       </main>
