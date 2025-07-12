ff0a68f533c459857dbde4da06ed6d404c4374da
xin <anxanxx@gmail.com>
2025-07-12 21:39:46 +0800

    feat(dependencies): add @base-ui-components/react dependency and update pnpm-lock.yaml

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <div className='root'>{children}</div>
         </ThemeProvider>
       </body>
     </html>
