a9939189955d9a7ca507aedb0ddb03a146381db5
xin <anxanxx@gmail.com>
2025-07-12 16:56:53 +0800

    add fumadocs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import { RootProvider } from 'fumadocs-ui/provider';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import './figma-styles.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <RootProvider>{children}</RootProvider>
       </body>
     </html>
   );
