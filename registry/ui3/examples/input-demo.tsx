a1e944f31618e162be31c2ff2d4fc70483a89f89
XIN <anxanxx@gmail.com>
2025-07-17 19:13:36 +0800

    refactor(input): replace TextInput with Input component in demo

--- registry/ui3/examples/input-demo.tsx
+++ registry/ui3/examples/input-demo.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { TextInput } from '@/registry/ui3/ui/input';
+import { Input } from '@/registry/ui3/ui/input';
 
 export default function InputDemo() {
   return (
     <div className='flex flex-col flex-wrap items-center gap-2'>
-      <TextInput placeholder='Singleline' className='w-20' />
-      <TextInput placeholder='Singleline large' size='large' className='w-32' />
-      <TextInput placeholder='Multiline' variant='multiline' className='w-32' />
+      <Input placeholder='Input' className='w-32' />
+      <Input placeholder='Input large' size='large' className='w-32' />
     </div>
   );
 }
