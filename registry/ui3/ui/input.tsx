e39e75748e92e791a9fcdd146ad53b059bec5570
XIN <60823033+anxndsgn@users.noreply.github.com>
2025-07-18 22:06:19 +0800

    fix(input): update input style

--- registry/ui3/ui/input.tsx
+++ registry/ui3/ui/input.tsx
@@ -4,16 +4,7 @@ import { cn } from '@/lib/utils';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 const inputVariants = cva(
-  [
-    'placeholder:text-(--color-text-tertiary)',
-    'border border-transparent selection:bg-(--color-bg-selected)',
-    'text-(--color-text) flex w-full min-w-0',
-    'rounded-(--radius-medium) bg-(--color-bg-secondary)',
-    'px-(--spacer-2) typography-body-medium outline-none',
-    'disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
-    'hover:border-(--color-border) focus-visible:border-(--color-border-selected)',
-    'aria-invalid:ring-destructive/20 aria-invalid:border-destructive',
-  ],
+  'placeholder:text-grey-400 border border-transparent selection:bg-blue-500 text-black-800 flex w-full min-w-0 rounded-medium bg-grey-100 px-2 typography-body-medium outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 hover:border-grey-200 focus-visible:border-blue-500 aria-invalid:ring-red-500/20 aria-invalid:border-red-500 dark:placeholder:text-grey-400 dark:bg-grey-700 dark:text-white-1000 dark:hover:border-grey-600 dark:focus-visible:border-blue-500',
   {
     variants: {
       size: {
