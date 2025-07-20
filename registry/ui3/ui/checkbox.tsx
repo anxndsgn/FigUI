fdb2768918a95ffeb25eb225c43d84acc49bec32
XIN <60823033+anxndsgn@users.noreply.github.com>
2025-07-20 15:03:09 +0800

    refactor: enhance checkbox styling and structure

--- registry/ui3/ui/checkbox.tsx
+++ registry/ui3/ui/checkbox.tsx
@@ -10,14 +10,13 @@ function Checkbox({
   return (
     <BaseCheckbox.Root
       className={cn(
-        'border-grey-200 bg-grey-100 data-[checked]:text-white-1000 size-4 shrink-0 rounded-md border outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed data-[checked]:border-blue-600 data-[checked]:bg-blue-500',
-        'dark:border-grey-600 dark:bg-grey-700 dark:data-[checked]:border-blue-400 dark:data-[checked]:bg-blue-500',
+        'group border-grey-200 bg-grey-100 data-[checked]:text-white-1000 dark:border-grey-600 dark:bg-grey-700 data-[disabled]:bg-grey-300 dark:data-[disabled]:bg-grey-500 data-[checked]:focus-visible:inset-ring-white-1000 size-4 shrink-0 rounded-md border outline-none focus-visible:border-blue-500 disabled:cursor-not-allowed data-[checked]:border-blue-600 data-[checked]:bg-blue-500 data-[checked]:focus-visible:inset-ring data-[disabled]:border-none dark:focus-visible:border-blue-400 dark:data-[checked]:border-blue-400 dark:data-[checked]:bg-blue-500',
         className,
       )}
       {...props}
     >
-      <BaseCheckbox.Indicator className='flex items-center justify-center text-current transition-none'>
-        <CheckIcon className='size-4 pb-0.5' />
+      <BaseCheckbox.Indicator className='group-data-[disabled]:text-white-1000 dark:group-data-[disabled]:text-grey-800 flex items-center justify-center text-current transition-none'>
+        <CheckIcon className='size-4 pb-[1px]' />
       </BaseCheckbox.Indicator>
     </BaseCheckbox.Root>
   );
