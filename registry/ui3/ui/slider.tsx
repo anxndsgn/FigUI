b3a7a39c5f8e93be792bae321359271a9a7a06c6
XIN <60823033+anxndsgn@users.noreply.github.com>
2025-07-26 15:22:51 +0800

    feat: update style

--- registry/ui3/ui/slider.tsx
+++ registry/ui3/ui/slider.tsx
@@ -29,13 +29,21 @@ function Slider({
       className={cn('px-2', className)}
       {...props}
     >
-      <BaseSlider.Control className='bg-grey-100 flex touch-none items-center select-none'>
-        <BaseSlider.Track className='bg-grey-100 h-4 w-full select-none'>
-          <BaseSlider.Indicator className='bg-blue-500 select-none' />
+      <BaseSlider.Control className='bg-grey-100 flex touch-none items-center rounded-full px-2 select-none'>
+        <BaseSlider.Track className={cn('bg-grey-100 h-4 w-full select-none')}>
+          <BaseSlider.Indicator
+            className={cn(
+              'bg-blue-500 select-none',
+              'before:absolute before:top-0 before:bottom-0 before:left-0 before:w-2 before:-translate-x-full before:rounded-l-full before:bg-blue-500 before:content-[""]',
+            )}
+          />
           {Array.from({ length: _values.length }).map((_, index) => (
             <BaseSlider.Thumb
               key={index}
-              className='shadow-300 bg-white-1000 size-4 rounded-full select-none'
+              className={cn(
+                'shadow-300 bg-white-1000 size-4 rounded-full select-none',
+                'before:border-black-100 before:absolute before:top-1/2 before:right-1/2 before:size-2 before:translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:border before:bg-blue-500 before:content-[""]',
+              )}
             />
           ))}
         </BaseSlider.Track>
