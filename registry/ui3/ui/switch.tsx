e1e1b2523e20d353fdac9c93305f9f51ede4d58b
XIN <60823033+anxndsgn@users.noreply.github.com>
2025-08-03 16:51:40 +0800

    fix(switch): update background color classes for unchecked state in light and dark modes

--- registry/ui3/ui/switch.tsx
+++ registry/ui3/ui/switch.tsx
@@ -1,6 +1,6 @@
+import { cn } from '@/lib/utils';
 import { Switch as BaseSwitch } from '@base-ui-components/react';
 import React from 'react';
-import { cn } from '@/lib/utils';
 
 export function Switch({
   className,
@@ -10,7 +10,7 @@ export function Switch({
     <BaseSwitch.Root
       {...props}
       className={cn(
-        'bg-grey-200 dark:bg-grey-600 data-disabled:bg-grey-300 dark:data-disabled:bg-grey-500 focus-visible:inset-ring-white-1000 relative inline-flex h-4 w-8 items-center rounded-full p-[1px] transition-colors duration-200 ease-in-out outline-none focus-visible:ring focus-visible:inset-ring-2 focus-visible:ring-blue-500 data-checked:bg-blue-500 dark:focus-visible:ring-blue-400',
+        'data-unchecked:bg-grey-200 dark:data-unchecked:bg-grey-600 data-disabled:bg-grey-300 dark:data-disabled:bg-grey-500 focus-visible:inset-ring-white-1000 relative inline-flex h-4 w-8 shrink-0 items-center rounded-full p-[1px] transition-colors duration-200 ease-in-out outline-none focus-visible:ring focus-visible:inset-ring-2 focus-visible:ring-blue-500 data-checked:bg-blue-500 dark:focus-visible:ring-blue-400',
         className,
       )}
     >
