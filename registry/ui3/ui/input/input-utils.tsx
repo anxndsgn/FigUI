38280702afc5327f22280eb64fdd6391144e9e10
XIN <60823033+anxndsgn@users.noreply.github.com>
2025-09-13 16:18:56 +0800

    fix(input): update StyledInputWrapper to use ring for focus and hover states instead of border

--- registry/ui3/ui/input/input-utils.tsx
+++ registry/ui3/ui/input/input-utils.tsx
@@ -10,7 +10,7 @@ function StyledInputWrapper({ className, children }: BaseInputProps) {
   return (
     <div
       className={cn(
-        'placeholder:text-grey-400 text-black-800 bg-grey-100 typography-body-medium hover:border-grey-200 dark:placeholder:text-grey-400 dark:bg-grey-700 dark:text-white-1000 dark:hover:border-grey-600 flex h-6 w-full min-w-0 items-center rounded-md border border-transparent pr-2 outline-none selection:bg-blue-500 focus-within:border-blue-500 hover:focus-within:border-blue-500 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-red-500 aria-invalid:ring-red-500/20 dark:focus-within:border-blue-500',
+        'placeholder:text-grey-400 text-black-800 bg-grey-100 typography-body-medium hover:ring-grey-200 dark:placeholder:text-grey-400 dark:bg-grey-700 dark:text-white-1000 dark:hover:ring-grey-600 flex h-6 w-full min-w-0 items-center rounded-md pr-2 ring ring-transparent outline-none selection:bg-blue-500 focus-within:ring-blue-500 hover:focus-within:ring-blue-500 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-red-500 aria-invalid:ring-red-500/20 dark:focus-within:ring-blue-500',
         className,
       )}
     >
