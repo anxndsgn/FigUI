bd9c3dc28143ee34567de26c486ec4f1412789ba
XIN <60823033+anxndsgn@users.noreply.github.com>
2025-09-13 16:07:18 +0800

    feat(input): update ColorInput demo to use controlled components and display selected color

--- registry/ui3/examples/color-input-demo.tsx
+++ registry/ui3/examples/color-input-demo.tsx
@@ -1,10 +1,29 @@
+'use client';
+
 import { ColorInput } from '@/registry/ui3/ui/input';
+import { useState } from 'react';
 
 export default function ColorInputDemo() {
+  const [color, setColor] = useState('000000');
   return (
     <div className='flex flex-col gap-2'>
-      <ColorInput className='w-32' defaultValue='000000' colorChit />
-      <ColorInput defaultValue='000000' opacity colorChit />
+      <ColorInput
+        className='w-32'
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+        colorChit
+      />
+      <ColorInput
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+        opacity
+        colorChit
+      />
+      <div
+        className='size-12 rounded-lg'
+        style={{ backgroundColor: `#${color}` }}
+      />
+      <span className='text-sm text-gray-500'>{color}</span>
     </div>
   );
 }
